6fbe30ce8b4ccc4ea2619d19e265d62573dd6986
KashbullinRail <kashbullinrail@gmail.com>
2022-12-09 16:06:07 +0300

    init Room in MainActivity.kt

--- app/src/main/java/com/example/roomexampleneco/MainActivity.kt
+++ app/src/main/java/com/example/roomexampleneco/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.roomexampleneco
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.asLiveData
 import com.example.roomexampleneco.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val dbRoom = MainDataBase.getDataBase(this@MainActivity)
+
+        dbRoom.getDao().getAllItem().asLiveData().observe(this){
+            it.forEach {
+                val text = "Id: ${it.id} Name: ${it.name} Price: ${it.price}\n"
+                binding.tvView.append(text)
+            }
+        }
+
+        with(binding){
+            btnSave.setOnClickListener {
+                val itemDbRoom = ItemEntity(
+                    null,
+                    etName.text.toString(),
+                    etPrice.text.toString()
+                    )
+
+                Thread {
+                    dbRoom.getDao().insertItem(itemDbRoom)
+                }.start()
+
+            }
+        }
+
+
+
     }
 }
